0a8d8a1f157457a0f3c9e00f9503b89d20808565
Luteces <ebompuis.dev@gmail.com>
2019-11-21 13:36:16 +0100

    Add new components into Vue

---
+++ Frond-end/pizerria_cde/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>{{title}}</h1>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    props: {
+      title: String
+    }
+
+  }
+
+</script>
+
+<style scoped>
+
+</style>
